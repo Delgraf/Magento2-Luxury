184c2073a73e47393234a2917b3fc46ceb10c129
Delgraf <info@delgraf.com>
2025-10-12 13:18:14 +0200

    Update custom.js

--- Swissup_Firecheckout/web/js/custom.js
+++ Swissup_Firecheckout/web/js/custom.js
@@ -2,23 +2,15 @@ define([
     'Swissup_Firecheckout/js/utils/form-field/manager',
     'Swissup_Firecheckout/js/utils/form-field/classname',
     'mage/translate'
-], function (
-    manager,
-    classname,
-    $t
-) {
+], function (manager, classname, $t) {
     'use strict';
-
 
     // Resize company to take the whole row
     classname('[name="company"]', 'fc-col-12');
-
 
     // `Street Address: Line 1` - rename to `Street Address`
     manager('[name="street[0]"]', {
         label: $t('Street Address'),
         placeholder: $t('Street Address *'),
     });
-
-
 });
